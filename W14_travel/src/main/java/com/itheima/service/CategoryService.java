9f6010d5b516b56595a3b6b2dd565beb27813710
ChenYH <woshicyh867572010@qq.com>
2019-07-01 10:53:52 +0800

    删除

--- W14_travel/src/main/java/com/itheima/service/CategoryService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface CategoryService {
-    String queryAll() throws JsonProcessingException;
-}
